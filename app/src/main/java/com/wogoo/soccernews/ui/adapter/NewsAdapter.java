e55e1cce9281bdbeb0a1d519a53d1795c2d17512
woggo <paulotimbu653@gmail.com>
2022-07-07 11:55:47 -0300

    rv

---
+++ app/src/main/java/com/wogoo/soccernews/ui/adapter/NewsAdapter.java
@@ -0,0 +1,51 @@
+package com.wogoo.soccernews.ui.adapter;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.wogoo.soccernews.databinding.NewsItemBinding;
+import com.wogoo.soccernews.domain.News;
+
+import java.util.List;
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+
+    private List<News> news;
+
+    public NewsAdapter(List<News> news) {
+        this.news = news;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        NewsItemBinding binding = NewsItemBinding.inflate(layoutInflater, parent, false);
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        News newsItem = news.get(position);
+        holder.binding.tvTitle.setText(newsItem.getTitle());
+        holder.binding.tvDesc.setText(newsItem.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.news.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final NewsItemBinding binding;
+
+        public ViewHolder(NewsItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+}
